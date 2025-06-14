f8b85041bf4a0aaa9de39a0ec96b15374487b685
Andres <andresmino1701@gmail.com>
2025-06-13 19:06:22 -0500

    Hamburguer

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -21,7 +21,7 @@ return (
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ duration: 0.2 }}
-                    className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 border-gray-200 dark:bg-gray-600"
+                    className="fixed left-0 rounded-xl shadow-4xl right-0 top-[3.5rem] p-5 pt-0 border-gray-200 dark:bg-gray-600"
                 >
                     <ul className="grid gap-2">
                         {routes.map((route, idx) => {
